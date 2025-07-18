d1e0535f4983604bfb2c06699b555eed2cb0b464
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2025-07-18 14:49:11 -0400

    rm config.toml, 95% sure it's ignored

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-nanoemoji assets/*.svg --config_file config.toml --output_file "Twemoji_COLRv1-16.0.1.ttf" --family "Twemoji COLRv1 Font"
+nanoemoji assets/*.svg --output_file "Twemoji_COLRv1-16.0.1.ttf" --family "Twemoji COLRv1 Font"
